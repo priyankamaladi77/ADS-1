49317eaf678877f44415090d6e4fbf72137fadce
priyankamaladi77 <svanmp@msitprogram.net>
2018-09-30 14:37:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+// /**
+//  * Linked Lists.
+//  */
+class LinkedList {
+    // /**
+    //  * Node variable.
+    //  */
+    private Node first;
+    // /**
+    //  * Class for node.
+    //  */
+    private class Node {
+        // /**
+        //  * character for item.
+        //  */
+        private char item;
+        // /**
+        //  * Node object for next.
+        //  */
+        private Node next;
+    }
+    /**
+     * Constructs the object.
+     */
+    LinkedList() {
+        this.first = null;
+    }
+    // /**
+    //  * Determines if empty.
+    //  *
+    //  * @return     True if empty, False otherwise.
+    //  */
+    public boolean isEmpty() {
+        return (first == null);
+    }
+    // /**
+    //  * to pop the element.
+    //  *
+    //  * @return     element on the top.
+    //  */
+    public char pop() {
+        if (first != null) {
+            char val = first.item;
+            first = first.next;
+            return val;
+        }
+        return '\0';
+    }
+    /**
+     * to push the element.
+     *
+     * @param      val  The val
+     */
+    public void push(final char val) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = val;
+        first.next = oldfirst;
+    }
+}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty.
+    }
+    // /**
+    //  * {Main method}.
+    //  *
+    //  * @param      args  The arguments
+    //  */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        int j = 0;
+        while (j < num) {
+            boolean val = check(sc);
+            if (val) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            j++;
+        }
+    }
+    /**
+     * {Method to check whether the parenthesis are balanced}.
+     *
+     * @param      scan  The scanner object
+     *
+     * @return     {Boolean}
+     */
+public static boolean check(final Scanner scan) {
+        boolean flag = false;
+        LinkedList list1 = new LinkedList();
+        String str = scan.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                list1.push(ch);
+            } else if (list1.isEmpty()) {
+                flag = false;
+                return flag;
+            } else if (ch == ')') {
+                if (list1 == null || list1.pop() != '(') {
+                    flag = false;
+                    return flag;
+                }
+            } else if (ch == ']') {
+                if (list1 == null || list1.pop() != '[') {
+                    flag = false;
+                    return flag;
+                }
+            } else if (ch == '}') {
+                if (list1 == null || list1.pop() != '{') {
+                    flag = false;
+                    // return flag;
+                }
+            }
+        }
+        if (list1.isEmpty()) {
+            flag = true;
+            return flag;
+        } else {
+            return flag;
+        }
+    }
+}
