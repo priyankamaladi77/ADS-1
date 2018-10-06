ec6c385afd6b415dbf3c6dd9e47203f6ac55c1db
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-06 12:55:10 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m12 mid xam 2/Assignment-1/Solution.java
+++ Assignments/m12 mid xam 2/Assignment-1/Solution.java
@@ -1,17 +1,51 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
+/**
+ * Class for insertion.
+ */
 class Insertion {
+    /**
+     * Constructs the object for insertion.
+     */
     private Insertion() {
+        //empty constructor.
     }
+    
+    /**
+     * { Exchanges the values of i and j in comparable array }.
+     *
+     * @param      arr   The comparable arr
+     * @param      i     the index
+     * @param      j     the index
+     * The time complexity is constant.
+     */
     private static void exchange(final Comparable[] arr,
         final int i, final int j) {
         Comparable swap = arr[i];
         arr[i] = arr[j];
         arr[j] = swap;
     }
+    
+    /**
+     * compares to comparable objects.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     returns -1 if true otherwise 0.
+     * The time complexity is constant.
+     */
     private static boolean less(final Comparable i, final Comparable j) {
          return (i.compareTo(j)) < 0;
     }
+    
+    /**
+     * sorts the given array using insertion sort.
+     *
+     * @param      arr   The comparable arr
+     * The time complexity is O(N^2).
+     */
     public static void sort(final Comparable[] arr) {
         int num = arr.length;
         for (int i = 0; i < num; i++) {
