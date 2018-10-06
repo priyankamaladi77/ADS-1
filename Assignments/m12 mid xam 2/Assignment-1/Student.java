d9c95ec54e62b353259383895d20dcbf0f8cd371
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-06 12:47:04 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m12 mid xam 2/Assignment-1/Student.java
+++ Assignments/m12 mid xam 2/Assignment-1/Student.java
@@ -1,217 +1,256 @@
+/**
+ * Class for student.
+ */
 class Student implements Comparable<Student> {
-	private String studentname;
-	private int dateofbirth;
-	private int s1;
-	private int s2;
-	private int s3;
-	private int total;
-	private String reserved;
-	private String unreserved;
+    /**
+     * { description of variable studentname }.
+     */
+    private String studentname;
+    /**
+     * { description of variable dateofbirth }.
+     */
+    private int dateofbirth;
+    /**
+     * { description of s1 }.
+     */
+    private int s1;
+    /**
+     * { description of variable s2 }.
+     */
+    private int s2;
+    /**
+     * { description of variable s3 }.
+     */
+    private int s3;
+    /**
+     * { description of variable total }.
+     */
+    private int total;
+    /**
+     * { description of variable reserved }.
+     */
+    private String reserved;
+    /**
+     * { description of variable unreserved }.
+     */
+    private String unreserved;
 
-	Student(final String name, final int birth, final int sub1,
-		final int sub2, final int sub3, final int tot, final String rese,
-		final String unrese) {
-		this.studentname = name;
-		this.dateofbirth = birth;
-		this.s1 = sub1;
-		this.s2 = sub2;
-		this.s3 = sub3;
-		this.total = tot;
-		this.reserved = rese;
-		this.unreserved = unrese;
-	}
+    /**
+     * Constructs the student object.
+     *
+     * @param      name    The name
+     * @param      birth   The birth
+     * @param      sub1    The sub 1
+     * @param      sub2    The sub 2
+     * @param      sub3    The sub 3
+     * @param      tot     The total
+     * @param      rese    The rese
+     * @param      unrese  The unrese
+     */
+    Student(final String name, final int birth, final int sub1,
+        final int sub2, final int sub3, final int tot,
+        final String rese, final String unrese) {
+        this.studentname = name;
+        this.dateofbirth = birth;
+        this.s1 = sub1;
+        this.s2 = sub2;
+        this.s3 = sub3;
+        this.total = tot;
+        this.reserved = rese;
+        this.unreserved = unrese;
+    }
 
-	/**
-	 * Gets the student name.
-	 *
-	 * @return     The student name.
-	 */
-	public String getStudentName() {
-		return this.studentname;
-	}
+    /**
+     * Gets the student name.
+     *
+     * @return     The student name.
+     */
+    public String getStudentName() {
+        return this.studentname;
+    }
 
-	/**
-	 * Gets the date of birth.
-	 *
-	 * @return     The date of birth.
-	 */
-	public int getDateOfBirth() {
-		return this.dateofbirth;
-	}
+    /**
+     * Gets the date of birth.
+     *
+     * @return     The date of birth.
+     */
+    public int getDateOfBirth() {
+        return this.dateofbirth;
+    }
 
-	/**
-	 * Gets the s 1.
-	 *
-	 * @return     The s 1.
-	 */
-	public int getS1() {
-		return this.s1;
-	}
+    /**
+     * Gets the s 1.
+     *
+     * @return     The s 1.
+     */
+    public int getS1() {
+        return this.s1;
+    }
 
-	/**
-	 * Gets the s 2.
-	 *
-	 * @return     The s 2.
-	 */
-	public int getS2() {
-		return this.s2;
-	}
+    /**
+     * Gets the s 2.
+     *
+     * @return     The s 2.
+     */
+    public int getS2() {
+        return this.s2;
+    }
 
-	/**
-	 * Gets the s 3.
-	 *
-	 * @return     The s 3.
-	 */
-	public int getS3() {
-		return this.s3;
-	}
+    /**
+     * Gets the s 3.
+     *
+     * @return     The s 3.
+     */
+    public int getS3() {
+        return this.s3;
+    }
 
-	/**
-	 * Gets the total.
-	 *
-	 * @return     The total.
-	 */
-	public int getTotal() {
-		return this.total;
-	}
+    /**
+     * Gets the total.
+     *
+     * @return     The total.
+     */
+    public int getTotal() {
+        return this.total;
+    }
 
-	/**
-	 * Gets the reserved.
-	 *
-	 * @return     The reserved.
-	 */
-	public String getReserved() {
-		return this.reserved;
-	}
+    /**
+     * Gets the reserved.
+     *
+     * @return     The reserved.
+     */
+    public String getReserved() {
+        return this.reserved;
+    }
 
-	/**
-	 * Gets the un reserved.
-	 *
-	 * @return     The un reserved.
-	 */
-	public String getUnReserved() {
-		return this.unreserved;
-	}
+    /**
+     * Gets the un reserved.
+     *
+     * @return     The un reserved.
+     */
+    public String getUnReserved() {
+        return this.unreserved;
+    }
 
-	/**
-	 * Sets the student name.
-	 *
-	 * @param      name  The name
-	 */
-	public void setStudentName( final String name) {
-		this.studentname = name;
-	}
+    /**
+     * Sets the student name.
+     *
+     * @param      name  The name
+     */
+    public void setStudentName(final String name) {
+        this.studentname = name;
+    }
 
-	/**
-	 * Sets the date of birth.
-	 *
-	 * @param      birth  The birth
-	 */
-	public void setDateOfBirth( final int birth) {
-		this.dateofbirth = birth;
-	}
+    /**
+     * Sets the date of birth.
+     *
+     * @param      birth  The birth
+     */
+    public void setDateOfBirth(final int birth) {
+        this.dateofbirth = birth;
+    }
 
-	/**
-	 * Sets the s 1.
-	 *
-	 * @param      sub1  The sub 1
-	 */
-	public void setS1( final int sub1) {
-		this.s1 = sub1;
-	}
+    /**
+     * Sets the s 1.
+     *
+     * @param      sub1  The sub 1
+     */
+    public void setS1(final int sub1) {
+        this.s1 = sub1;
+    }
 
-	/**
-	 * Sets the s 2.
-	 *
-	 * @param      sub2  The sub 2
-	 */
-	public void setS2( final int sub2) {
-		this.s2 = sub2;
-	}
+    /**
+     * Sets the s 2.
+     *
+     * @param      sub2  The sub 2
+     */
+    public void setS2(final int sub2) {
+        this.s2 = sub2;
+    }
 
-	/**
-	 * Sets the s 3.
-	 *
-	 * @param      sub3  The sub 3
-	 */
-	public void setS3( final int sub3) {
-		this.s3 = sub3;
-	}
+    /**
+     * Sets the s 3.
+     *
+     * @param      sub3  The sub 3
+     */
+    public void setS3(final int sub3) {
+        this.s3 = sub3;
+    }
 
-	/**
-	 * Sets the total.
-	 *
-	 * @param      tot   The total
-	 */
-	public void setTotal( final int tot) {
-		this.total = tot;
-	}
+    /**
+     * Sets the total.
+     *
+     * @param      tot   The total
+     */
+    public void setTotal(final int tot) {
+        this.total = tot;
+    }
 
-	/**
-	 * Sets the reserved.
-	 *
-	 * @param      rese  The rese
-	 */
-	public void setReserved( final String rese) {
-		this.reserved = rese;
-	}
-	/**
-	 * Sets the un reserved.
-	 *
-	 * @param      unrese  The unrese
-	 */
-	public void setUnReserved( final String unrese) {
-		this.unreserved = unrese;
-	}
+    /**
+     * Sets the reserved.
+     *
+     * @param      rese  The rese
+     */
+    public void setReserved(final String rese) {
+        this.reserved = rese;
+    }
+    /**
+     * Sets the un reserved.
+     *
+     * @param      unrese  The unrese
+     */
+    public void setUnReserved(final String unrese) {
+        this.unreserved = unrese;
+    }
 
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		return this.studentname;
-	}
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return this.studentname;
+    }
 
-	/**
-	 * { compares to comparable objects}.
-	 *
-	 * @param      that  The that
-	 *
-	 * @return     returns -1 and +1 based on comparision otherwise returns 0.
-	 */
-	public int compareTo(final Student that) {
-		if (this.total > that.total) {
-			return -1;
-		}
-		if (this.total < that.total) {
-			return +1;
-		}
-		if (this.s3 > that.s3) {
-			return -1;
-		}
-		if (this.s3 < that.s3) {
-			return +1;
-		}
-		if (this.s2 > that.s2) {
-			return -1;
-		}
-		if (this.s2 < that.s2) {
-			return +1;
-		}
-		if (this.dateofbirth < that.dateofbirth) {
-			return -1;
-		}
-		if (this.dateofbirth > that.dateofbirth) {
-			return +1;
-		}
+    /**
+     * { compares to comparable objects}.
+     *
+     * @param      that  The that
+     *
+     * @return     returns -1 and +1 based on comparision otherwise returns 0.
+     */
+    public int compareTo(final Student that) {
+        if (this.total > that.total) {
+            return -1;
+        }
+        if (this.total < that.total) {
+            return +1;
+        }
+        if (this.s3 > that.s3) {
+            return -1;
+        }
+        if (this.s3 < that.s3) {
+            return +1;
+        }
+        if (this.s2 > that.s2) {
+            return -1;
+        }
+        if (this.s2 < that.s2) {
+            return +1;
+        }
+        if (this.dateofbirth < that.dateofbirth) {
+            return -1;
+        }
+        if (this.dateofbirth > that.dateofbirth) {
+            return +1;
+        }
 
-		if (this.reserved.equals(that.reserved)) {
-			return -1;
-		}
-		if (this.unreserved.equals(that.unreserved)) {
-			return -1;
-		}
-		return 0;
-	}
+        if (this.reserved.equals(that.reserved)) {
+            return -1;
+        }
+        if (this.unreserved.equals(that.unreserved)) {
+            return -1;
+        }
+        return 0;
+    }
 }
