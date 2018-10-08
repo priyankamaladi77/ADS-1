52077bba16e2aa1c7dee66b410a291bf50e24e36
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-08 21:32:18 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m13/13.1 isMinHeap/Solution.java
+++ Assignments/m13/13.1 isMinHeap/Solution.java
@@ -6,84 +6,85 @@ import java.util.Scanner;
  * class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object for solution.
-	 * @author Priyanka maladi.
-	 */
-	private Solution() {
-		// empty constructor.
-	}
-	/**
-	 * { main function }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int n = Integer.parseInt(sc.nextLine());
-		switch (s) {
-			case "String":
-			while (n > 0) {
-				String[] tokens = sc.nextLine().split(",");
-				MinPQ<String> minimum = new
-				MinPQ<String>(tokens);
-				System.out.println(minimum.isMinPQ());
-				n--;
-			}
-			break;
-			case "Integer":
-			while (n > 0) {
-				String[] tokens = sc.nextLine().split(",");
-				Integer[] integer = new Integer[tokens.length];
-				for (int i = 0; i < tokens.length; i++) {
-					integer[i] =
-					Integer.parseInt(tokens[i]);
-				}
-				MinPQ<Integer> minimum = new
-				MinPQ<Integer>(integer);
-				System.out.println(minimum.isMinPQ());
-				n--;
-			}
-			break;
-			case "Double":
-			while (n > 0) {
-				String[] tokens = sc.nextLine().split(",");
-				Double[] doubtok = new Double[tokens.length];
-				for (int i = 0; i < tokens.length; i++) {
-					doubtok[i] =
-					Double.parseDouble(tokens[i]);
-				}
-				MinPQ<Double> minimum = new
-				MinPQ<Double>(doubtok);
-				System.out.println(minimum.isMinPQ());
-				n--;
-			}
-			break;
-			case "Float":
-			while (n > 0) {
-				String str = sc.nextLine();
+    /**
+     * Constructs the object for solution.
+     * @author Priyanka maladi.
+     */
+    private Solution() {
+        // empty constructor.
+    }
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = Integer.parseInt(sc.nextLine());
+        switch (s) {
+            case "String":
+            while (n > 0) {
+                String[] tokens = sc.nextLine().split(",");
+                MinPQ<String> minimum = new
+                MinPQ<String>(tokens);
+                System.out.println(minimum.isMinPQ());
+                n--;
+            }
+            break;
+            case "Integer":
+            while (n > 0) {
+                String[] tokens = sc.nextLine().split(",");
+                Integer[] integer = new Integer[tokens.length];
+                for (int i = 0; i < tokens.length; i++) {
+                    integer[i] =
+                    Integer.parseInt(tokens[i]);
+                }
+                MinPQ<Integer> minimum = new
+                MinPQ<Integer>(integer);
+                System.out.println(minimum.isMinPQ());
+                n--;
+            }
+            break;
+            case "Double":
+            while (n > 0) {
+                String[] tokens = sc.nextLine().split(",");
+                Double[] doubtok = new Double[tokens.length];
+                for (int i = 0; i < tokens.length; i++) {
+                    doubtok[i] =
+                    Double.parseDouble(tokens[i]);
+                }
+                MinPQ<Double> minimum = new
+                MinPQ<Double>(doubtok);
+                System.out.println(minimum.isMinPQ());
+                n--;
+            }
+            break;
+            case "Float":
+            while (n > 0) {
+                String str = sc.nextLine();
                 if (str.equals("")) {
                     System.out.println("false");
                     break;
                 } else {
-					String[] tokens = str.split(",");
-					Float[] floattok = new
-					Float[tokens.length];
-					for (int i = 0; i < tokens.length;
-						i++) {
-						floattok[i] =
-						Float.parseFloat(tokens[i]);				
-					}
-					MinPQ<Float> minimum = new
-					MinPQ<Float>(floattok);
-					System.out.println(minimum.isMinPQ());
-				}
-				n--;
-			}
-			break;
-		default:
-		break;
-		}
-	}
+                    String[] tokens = str.split(",");
+                    Float[] floattok = new
+                    Float[tokens.length];
+                    for (int i = 0; i < tokens.length;
+                        i++) {
+                        floattok[i] =
+                        Float.parseFloat(tokens[i]);          
+                    }
+                    MinPQ<Float> minimum = new
+                    MinPQ<Float>(floattok);
+                    System.out.println
+                    (minimum.isMinPQ());
+                }
+                n--;
+            }
+            break;
+        default:
+        break;
+        }
+    }
 }
