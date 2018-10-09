727bc44c53039181af6c60c373053d8368de8a02
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-09 17:38:41 +0530

    "ads1_m13_a1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m13/13.1 isMinHeap/MinPQ.java
+++ Assignments/m13/13.1 isMinHeap/MinPQ.java
@@ -26,11 +26,11 @@ class MinPQ<Q extends Comparable<Q>> {
         // for (int k = 1; k < arr.length - 1; k++) {
         //  if (less(2*k , k) && less(2*k + 1, k)) {
         for (int k = 1; k < arr.length / 2; k++) {
-                if ((less(2 * k, k)) && (less(2 * k + 1, k))) {
-                    return false;
+                if ((less(k, 2 * k)) && (less(k, 2 * k + 1))) {
+                    return true;
                 }
             }
-            return true;
+            return false;
         }
     /**
      * compares two elements of Q array.
