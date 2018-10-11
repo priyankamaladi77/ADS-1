20c0309c354f7f32c56f4d0c451ea7fddcfc1c21
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-11 22:18:21 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m10/10.1 LinkedList using Recursion/Linkedlist.java
+++ Assignments/m10/10.1 LinkedList using Recursion/Linkedlist.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 /**
  * constructs class for Linkedlist.
  */
@@ -109,51 +108,5 @@ class Linkedlist {
             }
         }
         return str.substring(0, str.length() - 2);
-    }
-}
-/**
- * Class for solution.
- */
-public final class Solution {
-    /**
-     * Constructs the object for solution.
-     */
-    private Solution() {
-        //empty constructor.
-    }
-    /**
-     * client program.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Linkedlist link = new Linkedlist();
-        while (sc.hasNext()) {
-            String[] tokens = sc.nextLine().split(" ");
-            switch (tokens[0]) {
-            case "insertAt":
-                if (Integer.parseInt(tokens[1]) < 0 || Integer.parseInt(
-                            tokens[1]) > link.size() + 1) {
-                    System.out.println("Can't insert at this position.");
-                } else {
-                    link.insertAt(Integer.parseInt(tokens[1]),
-                                   Integer.parseInt(tokens[2]));
-                    System.out.println(link.toString());
-                }
-                break;
-            case "reverse":
-                if (!link.isEmpty()) {
-                    link.reverse();
-                    System.out.println(link.toString());
-                } else {
-                    System.out.println("No elements to reverse.");
-                    break;
-                }
-            default:
-                break;
-            }
-        }
-
     }
 }
