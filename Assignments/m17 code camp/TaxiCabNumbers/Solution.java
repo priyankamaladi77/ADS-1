ea426f32897a7b38c60cf1ba37af5d462b2884db
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-12 21:37:30 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m17 code camp/TaxiCabNumbers/Solution.java
+++ Assignments/m17 code camp/TaxiCabNumbers/Solution.java
@@ -1,8 +1,23 @@
+/**
+ * imports scanner package.
+ */
 import java.util.Scanner;
+/**
+ * Class is used to find the nth taxi cab number with m pairs.
+ * @author Priyanka Maladi.
+ */
 public final class Solution {
+	/**
+	 * default constructor.
+	 */
 	private Solution() {
 		//unused constructor.
 	}
+	/**
+	 * To demonstarte the main solution.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
--- Assignments/m17 code camp/TaxiCabNumbers/TaxiCabNumbers.java
+++ Assignments/m17 code camp/TaxiCabNumbers/TaxiCabNumbers.java
@@ -1,13 +1,35 @@
 class TaxiCabNumbers implements Comparable<TaxiCabNumbers> {
+	/**
+	 * variable sum of type long.
+	 */
 	private long sum;
+	/**
+	 * variable i of type integer.
+	 */
 	private int i;
+	/**
+	 * variable j of type integer.
+	 */
 	private int j;
 
+	/**
+	 * Constructs the object for Taxi Cab Numbers.
+	 *
+	 * @param      i1    I 1
+	 * @param      j1    The j 1
+	 */
 	TaxiCabNumbers(int i1, int j1) {
 		this.sum = (long) (i1 * i1 * i1) + (j1 * j1 * j1);
 		this.i = i1;
 		this.j = j1;
 	}
+	/**
+	 * compares two objects.
+	 * Time complexity is constant as each statment executes only one.
+	 * @param      that  The that
+	 *
+	 * @return returns 1, -1 and 0 based on comparision.
+	 */
 	public int compareTo(TaxiCabNumbers that) {
 		if (this.sum > that.sum) {
 			return +1;
@@ -23,9 +45,23 @@ class TaxiCabNumbers implements Comparable<TaxiCabNumbers> {
 		}
 		return 0;
 	}
+	/** The time complexity is constant because only
+	 * one statment is executed.
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		return sum  + " : " + i + "^3 + " + j + "^3";
 	}
+	/**
+	 * To find the Taxi can numbers.
+	 * The time complexity is O(N) because for loop executes for N times.
+	 * @param      nth    number of accurance of taxicabnumber.
+	 * @param      mpair  number of pairs needed.
+	 *
+	 * @return     returns taxicabnumber.
+	 */
 	public static long findTaxiCabNumbers(final int nth, final int mpair) {
         MinPQ<TaxiCabNumbers> minPQ = new MinPQ<TaxiCabNumbers>();
         final int n = 600;
