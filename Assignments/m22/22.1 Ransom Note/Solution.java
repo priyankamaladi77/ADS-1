1fd7228ed0de4fab0f7a94c9b6d0a29a9cd45856
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-26 17:50:09 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m22/22.1 Ransom Note/Solution.java
+++ Assignments/m22/22.1 Ransom Note/Solution.java
@@ -6,33 +6,33 @@ import java.util.Scanner;
  * class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object for class solution.
-	 */
-	private Solution() {
-		//default constructor.
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		SeparateChainingHashST<String, Integer> obj
+    /**
+     * Constructs the object for class solution.
+     */
+    private Solution() {
+        //default constructor.
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SeparateChainingHashST<String, Integer> obj
         = new SeparateChainingHashST<String, Integer>();
-		String[] words = sc.nextLine().split(" ");
-		String[] line1 = sc.nextLine().split(" ");
-		for (int i = 0; i < line1.length; i++) {
-			if (obj.contains(line1[i])) {
-            	obj.put(line1[i], obj.get(line1[i]) + 1);
+        String[] words = sc.nextLine().split(" ");
+        String[] line1 = sc.nextLine().split(" ");
+        for (int i = 0; i < line1.length; i++) {
+            if (obj.contains(line1[i])) {
+                obj.put(line1[i], obj.get(line1[i]) + 1);
             } else {
                 obj.put(line1[i], 1);
             }
-		}
-		String[] line2 = sc.nextLine().split(" ");
-		for (int i = 0; i < line2.length; i++) {
-			if (obj.contains(line2[i])) {
+        }
+        String[] line2 = sc.nextLine().split(" ");
+        for (int i = 0; i < line2.length; i++) {
+            if (obj.contains(line2[i])) {
                 if (obj.get(line2[i]) == 0) {
                     System.out.println("No");
                     return;
@@ -43,7 +43,7 @@ public final class Solution {
                 System.out.println("No");
                 return;
             }
-		}
-		System.out.println("Yes");
-	}
+        }
+        System.out.println("Yes");
+    }
 }
