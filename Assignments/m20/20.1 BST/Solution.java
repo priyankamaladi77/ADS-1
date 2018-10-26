bd4241b91f9a70d088a858a4031d91ba138e8b84
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-26 17:27:13 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m20/20.1 BST/Solution.java
+++ Assignments/m20/20.1 BST/Solution.java
@@ -181,9 +181,15 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
     private Value get(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
-        if      (cmp < 0) return get(x.left, key);
-        else if (cmp > 0) return get(x.right, key);
-        else              return x.val;
+        if (cmp < 0) {
+            return get(x.left, key);
+        }
+        else if (cmp > 0) {
+            return get(x.right, key);
+        }
+        else {
+            return x.val;
+        }
     }
     /**
      * size function.
