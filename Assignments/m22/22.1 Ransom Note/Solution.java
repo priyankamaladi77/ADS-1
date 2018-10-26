01cc87dad66675d423f19bde94c817fa928d406b
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-26 18:06:18 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m22/22.1 Ransom Note/Solution.java
+++ Assignments/m22/22.1 Ransom Note/Solution.java
@@ -49,175 +49,139 @@ public final class Solution {
     }
 }
 /**
- * List of .
+ * Class for sequential search st.
  *
- * @param      <Item>  The item
+ * @param      <Key>    The key
+ * @param      <Value>  The value
  */
-class Queue<Item> implements Iterable<Item> {
-    /**
-     * { var_description }.
-     */
-    private Node<Item> first;
-    /**
-     * { var_description }.
-     */
-    private Node<Item> last;
-    /**
-     * { var_description }.
-     */
+class SequentialSearchST<Key, Value> {
     private int n;
+    private Node first;
 
-    /**
-     * Class for node.
-     *
-     * @param      <Item>  The item
-     */
-    private static class Node<Item> {
-        /**
-         * { var_description }.
-         */
-        private Item item;
-        /**
-         * { var_description }.
-         */
-        private Node<Item> next;
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node next;
+
+        public Node(Key key, Value val, Node next)  {
+            this.key  = key;
+            this.val  = val;
+            this.next = next;
+        }
     }
     /**
-     * Initializes an empty queue.
+     * Initializes an empty symbol table.
      */
-    Queue() {
-        first = null;
-        last  = null;
-        n = 0;
-    }
-    /**
-     * Returns true if this queue is empty.
-     *
-     * @return {@code true} if this queue is empty; {@code false} otherwise
-     */
-    public boolean isEmpty() {
-        return first == null;
+    public SequentialSearchST() {
     }
 
     /**
-     * Returns the number of items in this queue.
+     * Returns the number of key-value pairs in this symbol table.
      *
-     * @return the number of items in this queue
+     * @return the number of key-value pairs in this symbol table
      */
     public int size() {
         return n;
     }
 
     /**
-     * Returns the item least recently added to this queue.
+     * Returns true if this symbol table is empty.
      *
-     * @return     the item least recently added to this queue
+     * @return {@code true} if this symbol table is empty;
+     *         {@code false} otherwise
      */
-    public Item peek() {
-        return first.item;
+    public boolean isEmpty() {
+        return size() == 0;
     }
 
     /**
-     * Adds the item to this queue.
+     * Returns true if this symbol table contains the specified key.
      *
-     * @param  item the item to add
+     * @param      key   the key
+     *
+     * @return     {@code true} if this symbol table contains {@code key};
+     *             {@code false} otherwise
      */
-    public void enqueue(final Item item) {
-        Node<Item> oldlast = last;
-        last = new Node<Item>();
-        last.item = item;
-        last.next = null;
-        if (isEmpty()) {
-            first = last;
-        } else {
-            oldlast.next = last;
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     *
+     * @param      key   the key
+     *
+     * @return     the value associated with the given key if the key is in the
+     *             symbol table and {@code null} if the key is not in the symbol
+     *             table
+     */
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
         }
+        return null;
+    }
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting
+     * the old value with the new value if the symbol table already contains the
+     * specified key. Deletes the specified key (and its associated value) from
+     * this symbol table if the specified value is {@code null}.
+     *
+     * @param      key   the key
+     * @param      val   the value
+     */
+    public void put(Key key, Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
         n++;
     }
 
     /**
-     * Removes and returns the item on this queue that was least recently added.
+     * Removes the specified key and its associated value from this symbol table
+     * (if the key is in this symbol table).
      *
-     * @return     the item on this queue that was least recently added
+     * @param      key   the key
      */
-    public Item dequeue() {
-        Item item = first.item;
-        first = first.next;
-        n--;
-        if (isEmpty()) {
-            last = null;
-        }
-        return item;
+    public void delete(Key key) {
+        first = delete(first, key);
     }
 
-    /**
-     * Returns a string representation of this queue.
-     *
-     * @return the sequence of items in FIFO order, separated by spaces
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Item item : this) {
-            s.append(item);
-            s.append(' ');
+    // delete key in linked list beginning at Node x
+    // warning: function call stack too large if table is large
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            n--;
+            return x.next;
         }
-        return s.toString();
+        x.next = delete(x.next, key);
+        return x;
     }
 
-    /**
-     * Returns an iterator that iterates over.
-     *  the items in this queue in FIFO order.
-     *
-     * @return an iterator that iterates over
-     *  the items in this queue in FIFO order
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);
-    }
 
     /**
-     * Class for list iterator.
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
      *
-     * @param      <Item>  The item
+     * @return all keys in the symbol table
      */
-    private class ListIterator<Item> implements Iterator<Item> {
-        /**
-         * { var_description }.
-         */
-        private Node<Item> current;
-
-        /**
-         * Constructs the object.
-         *
-         * @param      first1  The first 1
-         */
-        ListIterator(final Node<Item> first1) {
-            current = first1;
-        }
-
-        /**
-         * Determines if it has next.
-         *
-         * @return     True if has next, False otherwise.
-         */
-        public boolean hasNext() {
-            return current != null;
-        }
-        /**
-         * { function_description }.
-         */
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-        /**
-         * { function_description }.
-         *
-         * @return     { description_of_the_return_value }
-         */
-        public Item next() {
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
+    public Iterable<Key> keys()  {
+        Queue<Key> queue = new Queue<Key>();
+        for (Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+        return queue;
     }
 }
