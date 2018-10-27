ad39e2a4bf24a6f1a053061defda95aed5e57756
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-27 12:32:15 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m24 final xam/1/Solution.java
+++ Assignments/m24 final xam/1/Solution.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * class for Solution.
  * @author Priyanka Maladi.
  */
-class Solution {
+final class Solution {
     /**
      * Constructs the object for Solution class.
      */
