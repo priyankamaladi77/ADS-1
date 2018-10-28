c5873ae4f961c6125c0697dc5794d278aaf412a0
priyankamaladi77 <svanmp@msitprogram.net>
2018-10-28 22:58:40 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
+++ Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
  * class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object for class solution.
-	 */
-	private Solution() {
-		//default constructor.
-	}
-	/**
-	 * main method for linear probing hash St.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+    /**
+     * Constructs the object for class solution.
+     */
+    private Solution() {
+        //default constructor.
+    }
+    /**
+     * main method for linear probing hash St.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         sc.nextLine();
         LinearProbingHashST<String, Integer> hst = new
         LinearProbingHashST<String, Integer>();
